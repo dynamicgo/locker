654881b62ac89795b25bca453220b68810dc9546
yayanyang <yayanyang@gmail.com>
2018-09-03 00:29:48 +0800

    null locker implement local process mutex semantic

--- null.go
+++ null.go
@@ -2,17 +2,21 @@ package locker
 
 import (
 	"context"
+	"sync"
 
 	config "github.com/dynamicgo/go-config"
 )
 
 type nullLocker struct {
+	sync.Mutex
 }
 
 func (locker *nullLocker) Lock(ctx context.Context) error {
+	locker.Mutex.Lock()
 	return nil
 }
 func (locker *nullLocker) Unlock(ctx context.Context) error {
+	locker.Mutex.Unlock()
 	return nil
 }
 
